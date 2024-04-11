026cc12320bbc854fc95574cca9f93cecffdb5a0
Akilesh S <akilesh0876@gmail.com>
2024-04-11 12:30:05 +0530

    Expert in WinUI since afternoon

--- Pages/PodkastPage.xaml.cs
+++ Pages/PodkastPage.xaml.cs
@@ -12,6 +12,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
 
 
 // To learn more about WinUI, the WinUI project structure,
@@ -23,12 +25,44 @@ namespace Podkast.Pages
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class PodkastPage : Page
-    {   
-
+    {
+        private StorageFile file;
         public PodkastPage()
         {
             this.InitializeComponent();
             
+        }
+        private async void PickAFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Clear previous returned file name, if it exists, between iterations of this scenario
+            PickAFileOutputTextBlock.Text = "";
+
+            // Create a file picker
+            var openPicker = new Windows.Storage.Pickers.FileOpenPicker();
+
+            // See the sample code below for how to make the window accessible from the App class.
+            var window = new MainWindow();
+
+            // Retrieve the window handle (HWND) of the current WinUI 3 window.
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
+
+            // Initialize the file picker with the window handle (HWND).
+            WinRT.Interop.InitializeWithWindow.Initialize(openPicker, hWnd);
+
+            // Set options for your file picker
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.FileTypeFilter.Add("*");
+
+            // Open the picker for the user to pick a file
+            file = await openPicker.PickSingleFileAsync();
+            if (file != null)
+            {
+                PickAFileOutputTextBlock.Text = "Picked file: " + file.Name;
+            }
+            else
+            {
+                PickAFileOutputTextBlock.Text = "Operation cancelled.";
+            }
         }
     }
 }
